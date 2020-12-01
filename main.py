ccf3719d63fb1fda7633e0e7b8a615bd1289d2ea
August Tollerup <augusttollerup@augusts-mbp.eduroam.wireless.dtu.dk>
2020-12-01 10:27:59 +0100

    Feat: Added live feature and corrected header-collection from last 150 mails from client

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
+"""
+Author: August Tollerup
+
+Feature to add:
+*
+Client based database and a combined database.
+
+Run the Anomaly detection on client based database and run visualising on the combined database.
+Allow the user/corporation to visualise their threat sources and where to reinforce deffence.
+*
+
+Collect the last 150 mails from a client and run Neural Network for Anomaly detection on these.
+Afterwards the systems should analyse each mail parallel to the clients operations.
+
+"""
 from utils import identifier as identify
+import utils
 import numpy as np
 import pandas as pd
+import re, platform
+import imaplib
 
-with open("email_source.txt", "r") as f:
-    source = f.read()
-    f.close()
 
-x = identify(source)
+live = False
+if platform.system() == "Windows":
+    live = True
+    import win32com.client
+    import pythoncom
 
-spf, dkim, dmarc, v_ratio, c_ratio = x.spf(), x.dkim(), x.dmarc(), x.ratio_char()[0], x.ratio_char()[1]
+utils.__init__()
+emails = utils.get_mails(1)
 
-# dataset = {"SPF": [1.], "DKIM": [1.], "DMARC": [1.], "Vowel Ratio": [0.29683698], "Consonant Ratio": [0.70316302]}
+print(len(emails))
 
-# df = pd.DataFrame (dataset, columns = ['SPF','DKIM', "DMARC", "Vowel Ratio", "Consonant Ratio"])
+def parse(mailobject):
+    x = identify(mailobject, live)
 
-df = pd.read_csv('data.csv', index_col=0)
+    spf, dkim, dmarc, v_ratio, c_ratio, sender = x.spf(), x.dkim(), x.dmarc(), x.ratio_char()[0], x.ratio_char()[1], x.sender_address()
 
-new_observation = [spf, dkim, dmarc, v_ratio, c_ratio]
+    urlScan = utils.scanURL(x.urls())
+    m_score = urlScan[0]
+    maliciousnes = urlScan[1]
 
-df.loc[len(df)+1] = new_observation
-print("Added new observation to row: {} in dataframe".format(len(df)))
+    df = pd.read_csv('data.csv', index_col=0)
 
-df.to_csv('data.csv')
+    new_observation = [spf, dkim, dmarc, v_ratio, c_ratio, sender, m_score, maliciousnes]
+
+    df.loc[len(df)+1] = new_observation
+    print("Added new observation to row: {} in dataframe".format(len(df)))
+
+    df.to_csv('data.csv')
+
+def run_live():
+    class Handler_Class(object):
+        """Endless outlook hook for fetching new emails from main inbox
+
+        Args:
+            Mail Object
+
+        Returns:
+            returns None
+        """
+
+        def OnNewMailEx(self, receivedItemsIDs):
+            for ID in receivedItemsIDs.split(","):
+                mail = outlook.Session.GetItemFromID(ID)
+                
+                print("parsing new email to database")
+                parse(mail)
+
+    outlook = win32com.client.DispatchWithEvents("Outlook.Application", Handler_Class)
+
+    #and then an infinit loop that waits from events.
+    pythoncom.PumpMessages()
--- utils.py
+++ utils.py
@@ -15,24 +15,95 @@ Roadblocks:
 - Convert from html to plaintext if HTML and not plain present (otherwise stick with plaintext)
 - Images???
 """
-import re, requests, json
+import requests, json, time, re, glob
+
+def get_mails(n):
+    import imaplib
+    import email
+    from email.header import decode_header
+    configs = open("config.txt")
+    configs = configs.read()
+    host = re.findall(r"host_outlook:(.+)", configs)[0]
+    email_ = re.findall(r"user:(.+)", configs)[0]
+    pword = re.findall(r"password:(.+)",configs)[0]
+    connection = imaplib.IMAP4_SSL(host)
+    connection.login(email_, pword)
+
+    connection.select("Inbox")
+    amount_of_recent_mails = n
+    raw_mail_headers = []
+    result, data = connection.uid('search', None, "ALL")
+    if result == 'OK':
+        for num in data[0].split()[:amount_of_recent_mails]:
+            result, data = connection.uid('fetch', num, '(RFC822)')
+            if result == 'OK':
+                email_message = email.message_from_bytes(data[0][1])
+                # print(email_message.__dict__)
+                print(str(email_message._headers))
+                raw_mail_headers.append(str(email_message._headers))
+    connection.close()
+    connection.logout()
+
+    return raw_mail_headers
 
 
-def scanURL(url):
-    headers = {'API-Key':'$apikey','Content-Type':'application/json'}
-    data = {"url": "https://urlyouwanttoscan.com/path/", "visibility": "public"}
-    response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
+def scanURL(urls):
+    score = 0
+    maliciousnes = 0
+    uuids = []
+    for url in urls:
+        print(url)
+        headers = {'API-Key':'920f2c76-d675-429c-8de5-8bfd04470847','Content-Type':'application/json'}
+        data = {"url": str(url), "visibility": "public"}
+        response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
+        uuid = response.json()["uuid"]
+        uuids.append(uuid)
+        print("https://urlscan.io/api/v1/result/"+str(uuid))
+        r = requests.get("https://urlscan.io/api/v1/result/"+str(uuid))
+        print(r.json())
+
+
+    time.sleep(16)
+    for uuid in uuids:
+        while True:
+            url = "https://urlscan.io/api/v1/result/"+str(uuid)
+            print(url)
+            r = requests.get(url)
+            if "message" in r.json().keys():
+                if r.json()["message"] == "Not Found" or r.json()["message"] == "notdone":
+                    time.sleep(5)
+                    continue
+            else:
+                # Can add more data
+                data = r.json()
+                score += data["verdicts"]["overall"]["score"]
+                if data["verdicts"]["overall"]["malicious"] == "false":
+                    pass
+                elif data["verdicts"]["overall"]["malicious"] == "true":
+                    maliciousnes += 1
+                break
+            
+    # Calculate mean of score and overall verdict of maliciousnes
+    m_score = (score/len(uuids))
+
+    return (m_score, maliciousnes)
 
 
 class identifier(object):
 
-    def __init__(self, source):
+    def __init__(self, object, live):
+        if live:
+            import win32com.client
+            import pythoncom
 
-        # For future functionality this should fetch from MIME object, nok via regex
-        regex = r"Content-type: text\/plain;\s+\S+\nContent-transfer-encoding: \S+\n\n((.|\n)*)Content-type:"
+        import re
 
-        self.source = source
-        self.mailbody = max(re.findall(regex, self.source)[0])
+        self.source = object.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x007D001F")
+        self.mailbody = object.Body
+        f = open("email_source.txt", "w")
+        f.write(self.source + "\n\n\n" + self.mailbody)
+        f.close()
+        self.sender = object.SenderEmailAddress
         
 
     # Fetch DKIM from the mail body
@@ -43,6 +114,7 @@ class identifier(object):
                 print("Found DKIM Pass")
                 return 1
             else:
+                print("no DKIM found")
                 return 0
         except Exception:
             return 0
@@ -55,6 +127,7 @@ class identifier(object):
                 print("Found DMARC none")
                 return 1
             else:
+                print("no DMARC found")
                 return 0
         except Exception:
             return 0
@@ -67,18 +140,25 @@ class identifier(object):
                 print("Found SPF Pass")
                 return 1
             else:
+                print("no SPF found")
                 return 0
         except Exception:
             return 0
 
     # Fetch all URLS from the mail body
     def urls(self):
-        regex = r'"https:\/\/eur05.safelinks.protection.outlook.com\/\?url=3?D?(\S)"'
-        urls = re.findall(regex, self.source)
+        regex = r"<https:\/\/eur05.safelinks.protection.outlook.com\/\?url=3?D?(\S)>"
+        regex_no_safelink = r"<(https?:\/\/\S+)>"
+        urls = re.findall(regex, self.mailbody)
+        urls += re.findall(regex_no_safelink, self.mailbody)
+        if len(urls) > 0:
+            print("Found {} urls".format(len(urls)))
 
-        return urls
+            return urls
+        else:
+            return False
 
-    # Needs to be tokenized on the mailbody and not the source
+    # Find a use for Okpai BM25 instead!
 
     def ratio_char(self):
         mailbody = self.mailbody.replace(" ", "").replace("\n", "")
@@ -92,7 +172,7 @@ class identifier(object):
 
         # Might need to be improved
         v_ratio, c_ratio = v/len(characters), c/len(characters)
-        
+        print("Found ratios v_ratio {}, c_ratio {}".format(v_ratio, c_ratio))
         return (v_ratio, c_ratio)
 
 
@@ -102,14 +182,12 @@ class identifier(object):
         regex_return_path = r"Return-Path:\s?(.+)"
         pass
 
-    pass
+    def sender_address(self):
+        print("Found sender: {}".format(self.sender))
+        return self.sender
+
+    #################################
+    # Add function to detect grammar errors. Wrongly spelled words, syntax (very complex)
+    #################################
 
 
-class formatter(object):
-
-    def __init__(self):
-        pass
-
-
-
-    pass
