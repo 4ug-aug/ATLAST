180f888abd30487cb985e85c732990b5d1db02d7
August Tollerup <augusttollerup@Augusts-MacBook-Pro.local>
2020-12-02 09:49:30 +0100

    Ready for automation of database

---
+++ commit.py
@@ -0,0 +1,7 @@
+import sys
+from subprocess import call
+
+call("git add data.csv", shell=True)
+
+call("git commit -m 'New Occurence in the database'", shell=True)
+call("git push -u origin main", shell=True)
